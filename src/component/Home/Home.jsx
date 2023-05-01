aee7d191111fd6adc5b04d20d02321d3fb1a7bf5
Sakibahmed2 <mauijaloskor@gmail.com>
2023-05-01 21:13:13 +0600

    nav and banner done

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h2>This is home page</h2>
+            <div className="hero mt-20 ">
+                <div className="hero-content flex-col lg:flex-row">
+                    <img src="https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg?w=740&t=st=1682952964~exp=1682953564~hmac=d272d427bf04c69faeb30bdcf9979b5f54062ad19fcc29cd740dc4d496843012" className="max-w-2xl rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 className="text-5xl font-bold">Welcome to 
+                            <span className='text-teal-400'>
+                                 Recipe master
+                            </span>
+                        </h1>
+                        <p className="py-6">Our food is made with high-quality ingredients that are carefully sourced and prepared to create delicious and nutritious meals. We offer a wide range of options to suit different tastes and dietary needs, including vegetarian, vegan, and gluten-free dishes.</p>
+                        <button className="my-btn">Login</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <div>
-            <h2>This is nav bar</h2>
+            <div className="py-4 bg-teal-500 text-white md:flex justify-between px-8 shadow-xl">
+                <h2 className=" font-semibold text-3xl">Recipe master</h2>
+                <div className='font-semibold text-xl'>
+                    <Link to="/">Home</Link>
+                    <Link to="/" className='mx-20'>Blog</Link>
+                    <Link to="/" >Blog</Link>
+                </div>
+            </div>
         </div>
     );
 };
