236f0754ec997524543ba67781746bc4b06a3098
Sakibahmed2 <mauijaloskor@gmail.com>
2023-05-01 22:10:09 +0600

    login register page done

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -3,7 +3,39 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h3>This is footer</h3>
+            <footer className="footer p-10 bg-base-200 text-base-content md:flex  md:justify-between justify-center">
+                <div className='w-[520px]'>
+                    <h4 className='text-4xl font-bold text-teal-500'>Recipe master</h4>
+                    <p className='text-lg text-gray-500 '>
+                        Our food is made with high-quality ingredients that are carefully sourced and prepared to create delicious and nutritious meals. We offer a wide range of options to suit different tastes and dietary needs, including vegetarian, vegan, and gluten-free dishes.
+                    </p>
+                </div>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Premium quality</a>
+                    <a className="link link-hover">100% Organic</a>
+                    <a className="link link-hover">Vegan free</a>
+                    <a className="link link-hover">Handmade</a>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                </div>
+                <div>
+                    <span className="footer-title">Contact</span>
+                    <div className="form-control w-80">
+                        <label className="label">
+                            <span className="label-text">Enter your email address</span>
+                        </label>
+                        <div className="relative">
+                            <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" />
+                            <button className="btn my-btn border-none absolute top-0 right-0 rounded-l-none">Subscribe</button>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </div>
     );
 };
--- src/component/Register/Register.jsx
+++ src/component/Register/Register.jsx
@@ -1,9 +1,52 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h2>This is register page</h2>
+        <div className="hero  bg-base-200">
+            <div className="hero-content flex-col ">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Register now</h1>
+
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form className="card-body w-96">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" placeholder="name" name='name' className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo URL</span>
+                            </label>
+                            <input type="text" placeholder="photo url" name='photo'  className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" placeholder="email" name='emial' required className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="text" placeholder="password" name='password' required className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn my-btn border-none">Login</button>
+                        </div>
+                        <label className="label">
+                            <Link to='/login' className="label-text-alt link link-hover">Already have account</Link>
+                        </label>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
